72735f9b3452866de3823144336f7ae9855e7018
GabrielKraus <gabrielalvarezkraus@gmail.com>
2022-06-22 20:13:05 -0300

    funcion post y put implementada

--- Services/FilmsServices.cs
+++ Services/FilmsServices.cs
@@ -13,8 +13,35 @@ public class FilmsServices: IFilmsServices
     {
         return context.Films;
     }
+    public async Task Save(Films film)
+    {
+        context.Add(film);
+        await context.SaveChangesAsync();
+    }
+    public async Task Update(Guid id, Films film)
+    {
+        var filmActual = context.Films.Find(id);
+
+        if (filmActual != null)
+        {
+            filmActual.GenreId = film.GenreId;
+            filmActual.Title = film.Title;
+            filmActual.Picture = film.Picture;
+            filmActual.Description = film.Description;
+            filmActual.ClassType = film.ClassType;
+            filmActual.Duration = film.Duration;
+            filmActual.AgeClassification = film.AgeClassification;
+            filmActual.Direction = film.Direction;
+            filmActual.Cast = film.Cast;
+            filmActual.ScriptWriters = film.ScriptWriters;
+
+            await context.SaveChangesAsync();
+        }
+    }
 }
 public interface IFilmsServices
 {
     IEnumerable<Films> Get();
+    Task Save(Films film);
+    Task Update(Guid id, Films film);
 }
